9ba25d83ad7eca347da9404c14a343c11b5b0d05
yasirkhn11 <yasirkhan9335@gmail.com>
2025-04-30 08:51:53 +0500

    update-authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,28 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-
-// // **Verify JWT Token**
-// exports.verifyToken = (req, res, next) => {
-//     const token = req.header("Authorization");
-//     if (!token) return res.status(401).json({ message: "Access Denied" });
-
-//     try {
-//         const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         res.status(400).json({ message: "Invalid Token" });
-//     }
-// };
-
-// // **Check User Role**
-// exports.checkRole = (roles) => (req, res, next) => {
-//     if (!roles.includes(req.user.role)) {
-//         return res.status(403).json({ message: "Forbidden: Insufficient permissions" });
-//     }
-//     next();
-// };
-
 
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
